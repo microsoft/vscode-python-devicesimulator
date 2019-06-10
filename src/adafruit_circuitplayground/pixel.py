23e3feb7a8df62fec2457bd6fe896f223b47eb12
Christella Cidolit <t-chcido@microsoft.com>
2019-06-09 21:23:39 -0700

    Methods to allow colors input in hexadecimal value

--- src/adafruit_circuitplayground/pixel.py
+++ src/adafruit_circuitplayground/pixel.py
@@ -14,17 +14,29 @@ class Pixel:
         self._state['pixels'][index] = self.extractPixelValue(val) 
 
     def extractPixelValue(self, val):
+        # Convert HEX to RGB
+        if type(val) is not tuple:
+            val = self.hex_to_rgb(val)
         # Check it's a valid tuple
         if len(val) != 3:
-            raise ValueError('The pixel value should be a tuple with 3 values.')
+            raise ValueError('The pixel value should be a tuple with 3 values between 0 and 255 or an hexadecimal color between #000000 and #FFFFFF.')
         # Convert to int
         val = tuple(map(int, val)) 
         # Prevent negative values
         if any(pix < 0 or pix > 255 for pix in val): 
-            raise ValueError('The pixel value should be in range 0, 255.')
+            raise ValueError('The pixel value should  between 0 and 255 or an hexadecimal color between #000000 and #FFFFFF.')
 
         return val
 
     def fill(self, val):
         for index in range(len(self._state['pixels'])):
             self._state['pixels'][index] = self.extractPixelValue(val) 
+
+    # Adapted from : https://pythonjunkie.wordpress.com/2012/07/19/convert-hex-color-values-to-rgb-in-python/
+    def hex_to_rgb(self, hexValue):
+        hexValue = hexValue.lstrip('#')
+        valueLength = len(hexValue)
+        if valueLength != 6:
+            raise ValueError('The pixel hexadicimal color value should be in range #000000 and #FFFFFF.')
+        return tuple(int(hexValue[i:i+valueLength//3], 16) for i in range(0, valueLength, valueLength//3))
+
