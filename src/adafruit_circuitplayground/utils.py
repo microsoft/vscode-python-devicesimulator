d40a112c990e7199dd64d87dbacb9a0e171cebf3
Jonathan Wang <t-jowang@microsoft.com>
2019-06-21 00:11:35 -0700

    Refactor slash stripping method

--- src/adafruit_circuitplayground/utils.py
+++ src/adafruit_circuitplayground/utils.py
@@ -5,12 +5,7 @@ def show(state):
     print(json.dumps(state) + '\0', end='', flush=True)
 
 def remove_leading_slashes(string):
-    forward_slash_count = string.count('/')
-    back_slash_count = string.count('\\')
-        
-    if forward_slash_count > 1:
-        string = string.lstrip('/')
-    elif back_slash_count > 0:
-        string = string.lstrip('\\')
+    string = string.lstrip('/')
+    string = string.lstrip('\\')
     
     return string
