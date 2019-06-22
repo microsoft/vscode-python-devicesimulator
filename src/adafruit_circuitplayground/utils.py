334e7682de0f84fc063de85fbd5bae3e019def82
Jonathan Wang <jonathanwangg@gmail.com>
2019-06-21 18:25:24 -0700

    Provide functionality for playing a .wav file (#20)
    PBI: 27951
    Task: 28656
    
    * Refactor slash stripping method
    
    * Refactor remove_leading_slashes()
    
    * Add checking to see if file name ends with .wav
    
    * Change displayed error to our error and provide meaningful error message

--- src/adafruit_circuitplayground/utils.py
+++ src/adafruit_circuitplayground/utils.py
@@ -2,5 +2,9 @@ import sys
 import json
 
 def show(state):
-    print(json.dumps(state) + '\0', end='')
-    sys.stdout.flush()
+    print(json.dumps(state) + '\0', end='', flush=True)
+
+def remove_leading_slashes(string):
+    string = string.lstrip('\\/')
+    
+    return string
