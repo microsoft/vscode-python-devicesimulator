27dc91546ce5ab07d13b80e6c66c9bf9ca7c3532
Luke Slevinsky <t-luslev@microsoft.com>
2019-07-08 15:14:27 -0700

    Update the way we run tests to the new way

--- src/test/suite/extension.test.ts
+++ src/test/suite/extension.test.ts
@@ -5,6 +5,7 @@
 
 // The module 'assert' provides assertion methods from node
 import * as assert from 'assert';
+// import { before } from 'mocha';
 
 // You can import and use all API from the 'vscode' module
 // as well as import your extension to test it
@@ -12,10 +13,12 @@ import * as assert from 'assert';
 // import * as myExtension from '../extension';
 
 // Defines a Mocha test suite to group tests of similar kind together
-suite("Extension Tests", function () {
+suite('Extension Test Suite', () => {
+    // before(() => {
+    //     vscode.window.showInformationMessage('Start all tests.');
+    // });
 
-    // Defines a Mocha unit test
-    test("Something 1", function() {
+    test('Sample test', () => {
         assert.equal(-1, [1, 2, 3].indexOf(5));
         assert.equal(-1, [1, 2, 3].indexOf(0));
     });
