a26e31306d722c66edad9999e8db2b7384de0f60
Luke Slevinsky <lslevins@ualberta.ca>
2019-07-09 13:33:56 -0700

    Update src/test/suite/extension.test.ts
    Co-Authored-By: DIGarnier <DIGarnier@users.noreply.github.com>

--- src/test/suite/extension.test.ts
+++ src/test/suite/extension.test.ts
@@ -5,7 +5,6 @@
 
 // The module 'assert' provides assertion methods from node
 import * as assert from 'assert';
-// import { before } from 'mocha';
 
 // You can import and use all API from the 'vscode' module
 // as well as import your extension to test it
