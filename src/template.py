2d5b5dcd14a1bb166bdc57e6acdfb0ed816b5938
Jonathan Wang <jonathanwangg@gmail.com>
2019-07-11 16:14:42 -0700

    Apply suggestions from code review
    Removed extra space and line-wrap URLs to try to avoid horizontal scrolling
    
    Co-Authored-By: Brett Cannon <54418+brettcannon@users.noreply.github.com>

--- src/template.py
+++ src/template.py
@@ -1,8 +1,10 @@
-""" Save your file as "code.py" or "main.py" to run on the actual device.
+"""Save your file as "code.py" or "main.py" to run on the actual device.
 
-Getting started with CPX and CircuitPython intro on: https://learn.adafruit.com/circuitpython-made-easy-on-circuit-playground-express/circuit-playground-express-library
+Getting started with CPX and CircuitPython intro on:
+https://learn.adafruit.com/circuitpython-made-easy-on-circuit-playground-express/circuit-playground-express-library
 
-Find example code for CPX on: https://github.com/adafruit/Adafruit_CircuitPython_CircuitPlayground/tree/master/examples
+Find example code for CPX on:
+https://github.com/adafruit/Adafruit_CircuitPython_CircuitPlayground/tree/master/examples
 """
 
 # import CPX library
