8e3d1f21f3e70ec68b0e178975b3dd625283f225
Fatou Mounzeo <t-famoun@microsoft.com>
2019-07-19 22:59:10 -0700

    used prettier

--- src/view/components/toolbar/InputSlider.tsx
+++ src/view/components/toolbar/InputSlider.tsx
@@ -2,118 +2,108 @@
 // Licensed under the MIT license.
 
 import * as React from "react";
-import "./InputSlider.css"
+import "./InputSlider.css";
 
 interface vscode {
-    postMessage(message: any): void;
+  postMessage(message: any): void;
 }
-  
+
 declare const vscode: vscode;
 
 const sendMessage = (state: any) => {
-    console.log("sendmessage");
-    vscode.postMessage({ command: "sensor-changed", text: state });
+  console.log("sendmessage");
+  vscode.postMessage({ command: "sensor-changed", text: state });
 };
-  
- 
-interface ISliderProps{
-    min:number;
-    max: number;
-    min_label: string;
-    max_label: string;
-    step:number;
-    type: string;
+
+interface ISliderProps {
+  min: number;
+  max: number;
+  min_label: string;
+  max_label: string;
+  step: number;
+  type: string;
 }
 
+class InputSlider extends React.Component<ISliderProps, any, any> {
+  constructor(props: ISliderProps) {
+    super(props);
+    this.state = {
+      value: 0
+    };
 
+    this.handleOnChange = this.handleOnChange.bind(this);
+    this.validateRange = this.validateRange.bind(this);
+  }
 
-class InputSlider extends React.Component<ISliderProps,any,any>{
-
-    constructor(props: ISliderProps){
-        super(props);
-        this.state = {
-            value: 0
-        };
-
-        this.handleOnChange = this.handleOnChange.bind(this);
-        this.validateRange = this.validateRange.bind(this);
-    }
-
-    
-    render(){
-      return (
-        <div className="inputSlider">
-            <input type="text"  className="sliderValue" value={this.state.value} 
-            onInputCapture={this.handleOnChange} defaultValue={this.props.min.toLocaleString()} pattern="[-?0-9]*" onKeyUp={this.validateRange}/>        
-            <div className="sliderArea">
-                <div className="upLabelArea">
-                    <div className='minLabel'>
-                        {this.props.min_label}
-                    </div>
-                    <div className='maxLabel'>
-                        {this.props.max_label}
-                    </div>
-                </div>
-                <input type="range"
-                    className="slider"
-                    min={this.props.min}
-                    max={this.props.max} 
-                    step={this.props.step} 
-                    onChange={this.handleOnChange}
-                    value={this.state.value}
-                    defaultValue={this.props.min.toLocaleString()}
-                />
-                <div className="downLabelArea">
-                    <div className='minLabel'>
-                        {this.props.min}
-                    </div>
-                    <div className='maxLabel'>
-                        {this.props.max}
-                    </div>
-                </div>
-            </div>
+  render() {
+    return (
+      <div className="inputSlider">
+        <input
+          type="text"
+          className="sliderValue"
+          value={this.state.value}
+          onInputCapture={this.handleOnChange}
+          defaultValue={this.props.min.toLocaleString()}
+          pattern="[-?0-9]*"
+          onKeyUp={this.validateRange}
+        />
+        <div className="sliderArea">
+          <div className="upLabelArea">
+            <div className="minLabel">{this.props.min_label}</div>
+            <div className="maxLabel">{this.props.max_label}</div>
+          </div>
+          <input
+            type="range"
+            className="slider"
+            min={this.props.min}
+            max={this.props.max}
+            step={this.props.step}
+            onChange={this.handleOnChange}
+            value={this.state.value}
+            defaultValue={this.props.min.toLocaleString()}
+          />
+          <div className="downLabelArea">
+            <div className="minLabel">{this.props.min}</div>
+            <div className="maxLabel">{this.props.max}</div>
+          </div>
         </div>
+      </div>
+    );
+  }
 
-        
-      )
+  private handleOnChange(event: React.ChangeEvent<HTMLInputElement>) {
+    this.updateValue(event);
+    this.validateRange();
+    console.log(this.state.value);
+    const newSensorState = this.setMessage(event);
+    if (newSensorState) {
+      sendMessage(newSensorState);
     }
-
-    private handleOnChange(event: React.ChangeEvent<HTMLInputElement>){
-       this.updateValue(event);
-       this.validateRange();
-       console.log(this.state.value)
-       const newSensorState = this.setMessage(event)
-       if(newSensorState){
-           sendMessage(newSensorState)
-       }
-       console.log(this.state.value)
-
+    console.log(this.state.value);
+  }
+  private setMessage(event: React.ChangeEvent<HTMLInputElement>) {
+    let newSensorState;
+    if (this.props.type && this.state.value && event.target.valueAsNumber) {
+      newSensorState = { temperature: event.target.valueAsNumber };
     }
-   private  setMessage(event: React.ChangeEvent<HTMLInputElement>) {
-        let newSensorState;
-        if(this.props.type && this.state.value && (event.target.valueAsNumber)){
-            newSensorState ={temperature: event.target.valueAsNumber}
-        }
-        return newSensorState
+    return newSensorState;
+  }
+
+  private updateValue(event: React.ChangeEvent<HTMLInputElement>) {
+    const newValue = event.target.validity.valid
+      ? event.target.value
+      : this.state.value;
+    this.setState({ value: newValue });
+  }
+
+  private validateRange() {
+    if (this.state.value < this.props.min) {
+      this.setState({ value: this.props.min, dummy: 2 });
     }
-
-    private updateValue(event: React.ChangeEvent<HTMLInputElement>){
-        const newValue = (event.target.validity.valid) ? event.target.value : this.state.value;
-        this.setState({value:newValue});
+    if (this.state.value > this.props.max) {
+      this.setState({ value: this.props.max, dummy: 1 });
     }
-
-    private validateRange(){
-        if(this.state.value<this.props.min){
-            this.setState({value:this.props.min,dummy:2});
-        }
-        if(this.state.value>this.props.max){
-            this.setState({value:this.props.max,dummy:1});
-        }
-
-    }
-
-
+  }
 }
 
 export default InputSlider;
-
