cb12da33b9ab790be888b3d21d9071cf1c70e9b9
Fatou Mounzeo <t-famoun@microsoft.com>
2019-07-30 09:48:42 -0700

    adding more svgs

---
+++ src/view/svgs/toolbar_svgs/slider_switch_svg.tsx
@@ -0,0 +1,35 @@
+import * as React from "react";
+
+export const SLIDER_SWITCH_SVG = (
+  <svg
+    width="20px"
+    height="16px"
+    viewBox="0 0 20 16"
+    version="1.1"
+    xmlns="http://www.w3.org/2000/svg"
+    xmlnsXlink="http://www.w3.org/1999/xlink"
+  >
+    <title>slider_switch</title>
+    <desc>Created with Sketch.</desc>
+    <g
+      id="slider_switch"
+      stroke="none"
+      stroke-width="1"
+      fill="none"
+      fill-rule="evenodd"
+    >
+      <g id="Group-10" transform="translate(0.000000, 3.000000)" fill="#FFFFFF">
+        <g id="Group-4">
+          <path
+            d="M5,0 L15,0 C17.7614237,-5.07265313e-16 20,2.23857625 20,5 C20,7.76142375 17.7614237,10 15,10 L5,10 C2.23857625,10 3.38176876e-16,7.76142375 0,5 C-3.38176876e-16,2.23857625 2.23857625,5.07265313e-16 5,0 Z M5,1.2 C2.90131795,1.2 1.2,2.90131795 1.2,5 C1.2,7.09868205 2.90131795,8.8 5,8.8 L15,8.8 C17.098682,8.8 18.8,7.09868205 18.8,5 C18.8,2.90131795 17.098682,1.2 15,1.2 L5,1.2 Z"
+            id="Rectangle"
+            fill-rule="nonzero"
+          />
+          <circle id="Oval" cx="5" cy="5" r="2.5" />
+        </g>
+      </g>
+    </g>
+  </svg>
+);
+
+export default SLIDER_SWITCH_SVG;
