9f1deda29880b0ce14d9e13aa962f156254e0192
Fatou Mounzeo <t-famoun@microsoft.com>
2019-07-30 16:36:47 -0700

    handling on click

--- src/view/components/toolbar/ToolBar.tsx
+++ src/view/components/toolbar/ToolBar.tsx
@@ -1,5 +1,5 @@
 import * as React from "react";
-import ModalButton from "../toolbar/ModalButton";
+import Button from "../Button";
 import * as TOOLBAR_SVG from "../../svgs/toolbar_svg";
 import "../../styles/ToolBar.css";
 import Modal from "../toolbar/SensorModal";
@@ -7,111 +7,108 @@ import Modal from "../toolbar/SensorModal";
 const TOOLBAR_BUTTON_WIDTH: number = 32;
 const TOOLBAR_EDGE_WIDTH: number = 8;
 
-class ToolBar extends React.Component {
+class ToolBar extends React.Component<any, any, any> {
+  constructor(props: any) {
+    super(props);
+    this.state = {
+      showModal: false
+    };
+    this.handleOnClick = this.handleOnClick.bind(this);
+  }
+
   render() {
     return (
       <div className="toolbar">
-        <ModalButton
+        <Button
           width={TOOLBAR_EDGE_WIDTH}
-          onClick={() => {}}
+          onClick={this.handleOnClick}
           image={TOOLBAR_SVG.EDGE_SVG}
           label="left_edge"
         />
-        <ModalButton
+        <Button
           width={TOOLBAR_BUTTON_WIDTH}
-          onClick={() => {}}
+          onClick={this.handleOnClick}
           image={TOOLBAR_SVG.SLIDER_SWITCH_SVG}
           label="temperature_sensor"
-        >
-          <Modal />
-        </ModalButton>
+        />
 
-        <ModalButton
+        <Button
           width={TOOLBAR_BUTTON_WIDTH}
-          onClick={() => {}}
+          onClick={this.handleOnClick}
           image={TOOLBAR_SVG.PUSH_BUTTON_SVG}
           label="motion_sensor"
-        >
-          <Modal />
-        </ModalButton>
+        />
 
-        <ModalButton
+        <Button
           width={TOOLBAR_BUTTON_WIDTH}
-          onClick={() => {}}
+          onClick={this.handleOnClick}
           image={TOOLBAR_SVG.RED_LED_SVG}
           label="light_sensor"
-        >
-          <Modal />
-        </ModalButton>
+        />
 
-        <ModalButton
+        <Button
           width={TOOLBAR_BUTTON_WIDTH}
-          onClick={() => {}}
+          onClick={this.handleOnClick}
           image={TOOLBAR_SVG.SOUND_SVG}
           label="temperature_sensor"
-        >
-          <Modal />
-        </ModalButton>
+        />
 
-        <ModalButton
+        <Button
           width={TOOLBAR_BUTTON_WIDTH}
-          onClick={() => {}}
+          onClick={this.handleOnClick}
           image={TOOLBAR_SVG.TEMPERATURE_SVG}
           label="motion_sensor"
-        >
-          <Modal />
-        </ModalButton>
+        />
 
-        <ModalButton
+        <Button
           width={TOOLBAR_BUTTON_WIDTH}
-          onClick={() => {}}
+          onClick={this.handleOnClick}
           image={TOOLBAR_SVG.LIGHT_SVG}
           label="light_sensor"
-        >
-          <Modal />
-        </ModalButton>
-        <ModalButton
+        />
+
+        <Button
           width={TOOLBAR_BUTTON_WIDTH}
-          onClick={() => {}}
+          onClick={this.handleOnClick}
           image={TOOLBAR_SVG.SPEAKER_SVG}
           label="temperature_sensor"
-        >
-          <Modal />
-        </ModalButton>
+        />
 
-        <ModalButton
+        <Button
           width={TOOLBAR_BUTTON_WIDTH}
-          onClick={() => {}}
+          onClick={this.handleOnClick}
           image={TOOLBAR_SVG.MOTION_SVG}
           label="motion_sensor"
-        >
-          <Modal />
-        </ModalButton>
+        />
 
-        <ModalButton
+        <Button
           width={TOOLBAR_BUTTON_WIDTH}
-          onClick={() => {}}
+          onClick={this.handleOnClick}
           image={TOOLBAR_SVG.IR_SVG}
           label="light_sensor"
-        >
-          <Modal />
-        </ModalButton>
-        <ModalButton
+        />
+
+        <Button
           width={TOOLBAR_BUTTON_WIDTH}
-          onClick={() => {}}
+          onClick={this.handleOnClick}
           image={TOOLBAR_SVG.GPIO_SVG}
           label="temperature_sensor"
-        >
-          <Modal />
-        </ModalButton>
-        <ModalButton
+        />
+
+        <Button
           width={TOOLBAR_EDGE_WIDTH}
-          onClick={() => {}}
+          onClick={this.handleOnClick("right_edge")}
           image={TOOLBAR_SVG.EDGE_SVG}
           label="right_edge"
         />
+        <Modal />
       </div>
     );
+  }
+
+  handleOnClick(label: string, event: React.MouseEvent<HTMLElement>) {
+    console.log(`"clicked on the button"${event.currentTarget.dataset}`);
+    this.setState({ showModal: !this.state.showModal });
   }
 }
 
