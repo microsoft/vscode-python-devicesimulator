2d74ddd5e9899ec4470852149791b9706b430d06
Fatou Mounzeo <t-famoun@microsoft.com>
2019-07-30 09:59:05 -0700

    adding icon svgs for toolbar

---
+++ src/view/svgs/toolbar_svgs/push_button_svg.tsx
@@ -0,0 +1,33 @@
+import * as React from "react";
+export const PUSH_BUTTON_SVG = (
+  <svg
+    width="16px"
+    height="16px"
+    viewBox="0 0 16 16"
+    version="1.1"
+    xmlns="http://www.w3.org/2000/svg"
+    xmlnsXlink="http://www.w3.org/1999/xlink"
+  >
+    \ <title>push_btn</title>
+    <desc>Created with Sketch.</desc>
+    <g
+      id="push_btn"
+      stroke="none"
+      stroke-width="1"
+      fill="none"
+      fill-rule="evenodd"
+    >
+      <g id="Group-10" transform="translate(1.000000, 1.000000)" fill="#FFFFFF">
+        <g id="Group-8">
+          <path
+            d="M2,0 L12,0 C13.1045695,-2.02906125e-16 14,0.8954305 14,2 L14,12 C14,13.1045695 13.1045695,14 12,14 L2,14 C0.8954305,14 1.3527075e-16,13.1045695 0,12 L0,2 C-1.3527075e-16,0.8954305 0.8954305,2.02906125e-16 2,0 Z M2,1.2 C1.5581722,1.2 1.2,1.5581722 1.2,2 L1.2,12 C1.2,12.4418278 1.5581722,12.8 2,12.8 L12,12.8 C12.4418278,12.8 12.8,12.4418278 12.8,12 L12.8,2 C12.8,1.5581722 12.4418278,1.2 12,1.2 L2,1.2 Z"
+            id="Rectangle"
+            fill-rule="nonzero"
+          />
+          <circle id="Oval" cx="7" cy="7" r="3.5" />
+        </g>
+      </g>
+    </g>
+  </svg>
+);
+export default PUSH_BUTTON_SVG;
