224ae7f90f6bde79df70b6a6bb967d759f594cf5
Fatou Mounzeo <t-famoun@microsoft.com>
2019-07-30 09:20:08 -0700

    adding svgs

--- src/view/svgs/toolbar_svgs/edge_svg.tsx
+++ src/view/svgs/toolbar_svgs/edge_svg.tsx
@@ -1,23 +1,23 @@
 import * as React from "react";
 
-export const TOOLBAR_EDGE_SVG = (
-    <svg
-        width="4"
-        height="16"
-        viewBox="0 0 4 16"
-        version="1.1" 
-        fill="none"
-        xmlns="http://www.w3.org/2000/svg"
-    >
-        <rect y="2.17143" width="4" height="0.8" rx="0.4" fill="#292929"/>
-        <rect width="4" height="0.8" rx="0.4" fill="#292929"/>
-        <rect y="4.34286" width="4" height="0.8" rx="0.4" fill="#292929"/>
-        <rect y="6.51428" width="4" height="0.8" rx="0.4" fill="#292929"/>
-        <rect y="8.68571" width="4" height="0.8" rx="0.4" fill="#292929"/>
-        <rect y="10.8571" width="4" height="0.8" rx="0.4" fill="#292929"/>
-        <rect y="13.0286" width="4" height="0.8" rx="0.4" fill="#292929"/>
-        <rect y="15.2" width="4" height="0.8" rx="0.4" fill="#292929"/>
-    </svg>
-  );
-  
-  export default TOOLBAR_EDGE_SVG;
+export const EDGE_SVG = (
+  <svg
+    width="4"
+    height="16"
+    viewBox="0 0 4 16"
+    version="1.1"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <rect y="2.17143" width="4" height="0.8" rx="0.4" fill="#292929" />
+    <rect width="4" height="0.8" rx="0.4" fill="#292929" />
+    <rect y="4.34286" width="4" height="0.8" rx="0.4" fill="#292929" />
+    <rect y="6.51428" width="4" height="0.8" rx="0.4" fill="#292929" />
+    <rect y="8.68571" width="4" height="0.8" rx="0.4" fill="#292929" />
+    <rect y="10.8571" width="4" height="0.8" rx="0.4" fill="#292929" />
+    <rect y="13.0286" width="4" height="0.8" rx="0.4" fill="#292929" />
+    <rect y="15.2" width="4" height="0.8" rx="0.4" fill="#292929" />
+  </svg>
+);
+
+export default EDGE_SVG;
