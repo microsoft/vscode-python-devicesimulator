bfa6c9698c797bd9f8c18930965349b00880f9f0
Fatou Mounzeo <t-famoun@microsoft.com>
2019-07-31 19:57:12 -0700

    adding titles and text to modal

--- src/view/components/toolbar/SensorModal.tsx
+++ src/view/components/toolbar/SensorModal.tsx
@@ -3,7 +3,8 @@
 import * as React from "react";
 import {
   LABEL_TO_MODAL_CONTENT,
-  DEFAULT_MODAL_CONTENT
+  DEFAULT_MODAL_CONTENT,
+  IModalContent
 } from "../component_utils";
 
 class SensorModal extends React.Component<any, any, any> {
@@ -17,16 +18,31 @@ class SensorModal extends React.Component<any, any, any> {
 
       if (
         this.props.showModal &&
-        this.props.label == label &&
+        this.props.label === label &&
         LABEL_TO_MODAL_CONTENT.get(label)
       ) {
-        const content = LABEL_TO_MODAL_CONTENT.get(label);
+        const content = LABEL_TO_MODAL_CONTENT.get(label) as IModalContent;
+
         console.log(`we have ${this.props.label} ${content}`);
 
         const component = content
           ? content["component"]
           : DEFAULT_MODAL_CONTENT.component;
-        return <div>{component}</div>;
+        return (
+          <div className="sensor_modal">
+            <div className="description_area">
+              <div className="title">{content["descriptionTitle"]}</div>
+              <div className="tag">{content["tag"]}</div>
+              <div className="description">{content["descriptionText"]}</div>
+            </div>
+            {/* make border visivle bottom */}
+            <div className="try_area">
+              <div className="title"> {content["tryItTitle"]}</div>
+              <div className="description">{content["tryItDescriptrion"]}</div>
+              <div>{component}</div>
+            </div>
+          </div>
+        );
       }
     }
     console.log(`was false`);
