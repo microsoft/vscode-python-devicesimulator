1f5cd80c0cd138007860e597306f0e4b99da07ca
Christellah <christella.cidolit@polymtl.ca>
2019-08-01 11:44:41 -0700

    Opening the simulator webview after the template file is opened (#85)
    Fixing BUG 31513

--- src/extension.ts
+++ src/extension.ts
@@ -180,19 +180,19 @@ export function activate(context: vscode.ExtensionContext) {
               telemetryAI.trackFeatureUsage(
                 TelemetryEventName.CLICK_DIALOG_TUTORIALS
               );
-            }
+            };
             utils.showPrivacyModal(okAction);
           }
         });
     }
 
-    openWebview();
-
     // tslint:disable-next-line: ban-comma-operator
     vscode.workspace
       .openTextDocument({ content: file, language: "python" })
       .then((template: vscode.TextDocument) => {
-        vscode.window.showTextDocument(template, 1, false);
+        vscode.window.showTextDocument(template, 1, false).then(() => {
+          openWebview();
+        });
       }),
       // tslint:disable-next-line: no-unused-expression
       (error: any) => {
@@ -408,7 +408,7 @@ export function activate(context: vscode.ExtensionContext) {
                       telemetryAI.trackFeatureUsage(
                         TelemetryEventName.CLICK_DIALOG_HELP_DEPLOY_TO_DEVICE
                       );
-                    }
+                    };
                     utils.showPrivacyModal(okAction);
                   }
                 });
