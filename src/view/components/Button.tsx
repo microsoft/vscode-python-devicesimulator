d71ce3b297110c900588e766dbb8401e32f08a88
Luke Slevinsky <t-luslev@microsoft.com>
2019-08-02 14:26:11 -0700

    remove double button

--- src/view/components/Button.tsx
+++ src/view/components/Button.tsx
@@ -17,7 +17,7 @@ const Button: React.FC<IButtonProps> = props => {
     <button
       id={`${props.label}-button`}
       className={`${props.label}-button button`}
-      aria-label={`${props.label} button`}
+      aria-label={props.label}
       role="button"
       onClick={props.onClick}
       style={buttonStyle}
