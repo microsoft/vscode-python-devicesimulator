45fb914ab092fc4cb3f16883f088a794e9dc3dd6
Christella Cidolit <t-chcido@microsoft.com>
2019-08-07 11:24:06 -0700

    Testing auto_write fix

--- src/adafruit_circuitplayground/utils.py
+++ src/adafruit_circuitplayground/utils.py
@@ -3,12 +3,22 @@
 
 import sys
 import json
+import copy
+import time
+
+
+previousState = {}
+TIME_DELAY = 0.01
 
 
 def show(state):
-    message = {'type': 'state', 'data': json.dumps(state)}
-    print(json.dumps(message) + '\0', end='')
-    sys.stdout.flush()
+    global previousState
+    if state != previousState:
+        message = {'type': 'state', 'data': json.dumps(state)}
+        print(json.dumps(message) + '\0', end='')
+        sys.stdout.flush()
+        previousState = copy.deepcopy(state)
+        time.sleep(TIME_DELAY)
 
 
 def remove_leading_slashes(string):
