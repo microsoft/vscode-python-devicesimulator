2e0129b768f40b69550a0e3796801e692b0a6bc2
Fatou Mounzeo <t-famoun@microsoft.com>
2019-08-07 20:47:10 -0700

    attemot using scipy

--- src/adafruit_circuitplayground/toner.py
+++ src/adafruit_circuitplayground/toner.py
@@ -1,5 +1,15 @@
 import threading
 from pysine import sine
+from scipy.io.wavfile import write
+import numpy as np
+import struct
+from . import utils
+import sys
+import os
+import simpleaudio as sa
+BIT_RATE = 44100
+
+FILENAME = "tone.wav"
 
 
 class TonerThread(threading.Thread):
@@ -10,7 +20,9 @@ class TonerThread(threading.Thread):
         super(TonerThread, self).__init__()
         self._stop_event = threading.Event()
         self._start_event = threading.Event()
+        self._play_tone_event = threading.Event()
         self.frequency = 440
+        self.duration = 0.2
 
     def stop_tone(self):
         if self._start_event.is_set():
@@ -18,9 +30,16 @@ class TonerThread(threading.Thread):
         self._stop_event.set()
 
     def start_tone(self):
+        self.write_wave_file(self.frequency, 0.2)
         if self._stop_event.is_set():
             self._stop_event.clear()
         self._start_event.set()
+
+    def play_tone(self):
+        # print('attempt at writing', flush=True)
+        # # self.write_wave_file(self.frequency, self.duration)
+        # print('finished writing', flush=True)
+        self._play_tone_event.set()
 
     def stopped(self):
         return self._stop_event.is_set()
@@ -28,7 +47,43 @@ class TonerThread(threading.Thread):
     def run(self):
         while True:
             if self._start_event.is_set() and not self.stopped():
-                sine(frequency=self.frequency, duration=0.2)
+                utils.play_wave_file(
+                    self._get_filename_absolute_path(FILENAME))
+            elif self._play_tone_event.is_set():
+                print('should play', flush=True)
+                print(self._get_filename_absolute_path(FILENAME))
+                # utils.play_wave_file(
+                #     self._get_filename_absolute_path(FILENAME))
+                self.createTone(self.frequency, self.duration)
+                self._play_tone_event.clear()
+
+    def _get_filename_absolute_path(self, file_name):
+        abs_path_to_code_file = ''
+        abs_path_parent_dir = os.path.abspath(
+            os.path.join(abs_path_to_code_file, os.curdir))
+        abs_path_wav_file = os.path.normpath(
+            os.path.join(abs_path_parent_dir, file_name))
 
     def set_frequency(self, frequency):
         self.frequency = frequency
+
+    def set_duration(self, duration):
+        self.duration = duration
+
+    def write_wave_file(self, frequency, duration):
+        each_sample_number = np.arange(duration * BIT_RATE)
+        waveform = np.sin(2 * np.pi * each_sample_number *
+                          frequency / BIT_RATE)
+        waveform_attenuated = waveform * 0.5
+        waveform_integers = np.int16(waveform_attenuated * 32767)
+        print("writing")
+        print(FILENAME)
+        write(FILENAME, BIT_RATE, waveform_integers)
+
+    def createTone(self, frequency, duration):
+        each_sample_number = np.arange(duration * BIT_RATE)
+        waveform = np.sin(2 * np.pi * each_sample_number *
+                          frequency / BIT_RATE)
+        waveform_attenuated = waveform * 0.5
+        waveform_integers = np.int16(waveform_attenuated * 32767)
+        play_obj = sa.play_buffer(waveform_integers, 1, 2, BIT_RATE)
--- src/adafruit_circuitplayground/utils.py
+++ src/adafruit_circuitplayground/utils.py
@@ -3,6 +3,7 @@
 
 import sys
 import json
+from playsound import playsound
 
 
 def show(state):
@@ -14,3 +15,20 @@ def show(state):
 def remove_leading_slashes(string):
     string = string.lstrip('\\/')
     return string
+
+
+def play_wave_file(file_name):
+
+    if sys.implementation.version[0] >= 3:
+        if file_name.endswith(".wav"):
+            try:
+                playsound(abs_path_wav_file)
+            except:
+                # TODO TASK: 29054 Verfication of a "valid" .wav file
+                raise EnvironmentError(
+                    "Your .wav file is not suitable for the Circuit Playground Express.")
+                print("writing file failed", flush=True)
+        else:
+            raise TypeError(file_name + " is not a path to a .wav file.")
+    else:
+        raise NotImplementedError("Please use Python 3 or higher.")
