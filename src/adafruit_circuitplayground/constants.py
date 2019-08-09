b6f737e8ff62af5fa71c39b8ec19c657a649abed
Luke Slevinsky <t-luslev@microsoft.com>
2019-08-09 00:33:20 -0700

    move constants into file

---
+++ src/adafruit_circuitplayground/constants.py
@@ -0,0 +1,16 @@
+# Copyright (c) Microsoft Corporation.
+# Licensed under the MIT license.
+
+ASSIGN_PIXEL_TYPE_ERROR = "The pixel color value type should be tuple, list or hexadecimal."
+
+BRIGHTNESS_RANGE_ERROR = "The brightness value should be a number between 0 and 1."
+
+INDEX_ERROR = "The index is not a valid number, you can access the Neopixels from 0 to 9."
+
+NOT_IMPLEMENTED_ERROR = "This method is not implemented by the simulator"
+
+NOT_SUITABLE_FILE_ERROR = "Your .wav file is not suitable for the Circuit Playground Express."
+
+PIXEL_RANGE_ERROR = "The pixel hexadicimal color value should be in range #000000 and #FFFFFF."
+
+VALID_PIXEL_ASSIGN_ERROR = "The pixel color value should be a tuple with three values between 0 and 255 or an hexadecimal color between 0x000000 and 0xFFFFFF."
--- src/process_user_code.py
+++ src/process_user_code.py
@@ -9,18 +9,7 @@ import copy
 from adafruit_circuitplayground.express import cpx
 from pathlib import Path
 import traceback
-
-EXPECTED_INPUT_EVENTS = [
-    'button_a',
-    'button_b',
-    'switch',
-    'temperature',
-    'light',
-    'shake',
-    'motion_x',
-    'motion_y',
-    'motion_z'
-]
+import python_constants as CONSTANTS
 
 read_val = ""
 
@@ -37,19 +26,19 @@ class UserInput(threading.Thread):
             sys.stdin.flush()
             try:
                 new_state = json.loads(read_val)
-                for event in EXPECTED_INPUT_EVENTS:
+                for event in CONSTANTS.EXPECTED_INPUT_EVENTS:
                     cpx._Express__state[event] = new_state.get(
                         event, cpx._Express__state[event])
 
             except Exception as e:
-                print("Error trying to send event to the process : ",
+                print(CONSTANTS.ERROR_SENDING_EVENT,
                       e, file=sys.stderr, flush=True)
 
 
 # Insert absolute path to Adafruit library into sys.path
 abs_path_to_parent_dir = os.path.dirname(os.path.abspath(__file__))
-library_name = "adafruit_circuitplayground"
-abs_path_to_lib = os.path.join(abs_path_to_parent_dir, library_name)
+abs_path_to_lib = os.path.join(
+    abs_path_to_parent_dir, CONSTANTS.LIBRARY_NAME)
 sys.path.insert(0, abs_path_to_lib)
 
 threads = []
@@ -66,12 +55,13 @@ def execute_user_code(abs_path_to_code_file):
     with open(abs_path_to_code_file) as file:
         user_code = file.read()
         try:
-            codeObj = compile(user_code, abs_path_to_code_file, 'exec')
+            codeObj = compile(user_code, abs_path_to_code_file,
+                              CONSTANTS.EXEC_COMMAND)
             exec(codeObj)
             sys.stdout.flush()
         except Exception as e:
             exc_type, exc_value, exc_traceback = sys.exc_info()
-            errorMessage = '\n\tTraceback of code execution : \n'
+            errorMessage = CONSTANTS.ERROR_TRACEBACK
             stackTrace = traceback.format_exception(
                 exc_type, exc_value, exc_traceback)
 
