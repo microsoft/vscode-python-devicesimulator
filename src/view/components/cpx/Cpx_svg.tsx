93046cf209240548299ed2c7b86eb69546615791
Fatou Mounzeo <t-famoun@microsoft.com>
2019-08-19 16:20:08 -0700

    ff

--- src/view/components/cpx/Cpx_svg.tsx
+++ src/view/components/cpx/Cpx_svg.tsx
@@ -9,7 +9,7 @@ export const CPX_SVG = (
   <svg
     xmlns="http://www.w3.org/2000/svg"
     id="cpx_svg"
-    // width="360"
+    width="45%"
     // height="360"
     viewBox="0 0 180.094 179.229"
   >
