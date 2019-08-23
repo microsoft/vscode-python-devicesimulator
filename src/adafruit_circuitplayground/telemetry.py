5a249d1fe384ecbd3d1c873978610036bc7bdf47
Fatou Mounzeo <t-famoun@microsoft.com>
2019-08-23 10:49:28 -0700

    removed print statement

--- src/adafruit_circuitplayground/telemetry.py
+++ src/adafruit_circuitplayground/telemetry.py
@@ -20,10 +20,7 @@ class Telemetry:
         self.extension_name = '__EXTENSIONNAME__'
 
     def send_telemetry(self, event_name):
-        print('entred telem sending {} with {}'.format(
-            event_name, self.__enable_telemetry), flush=True)
         if self.telemetry_available() and not self.telemetry_state[event_name] and self.__enable_telemetry:
-            print('both true {}'.format(event_name), flush=True)
             self.telemetry_client.track_event(
                 '{}/{}'.format(self.extension_name, CONSTANTS.TELEMETRY_EVENT_NAMES[event_name]))
             self.telemetry_client.flush()
