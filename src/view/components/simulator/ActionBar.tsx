f260ce8be895d2f06c9d48036681e72f1d181bd9
xnkevinnguyen <xuannamkevin@gmail.com>
2020-01-21 14:07:02 -0800

    Refactor css property of toolbar

--- src/view/components/simulator/ActionBar.tsx
+++ src/view/components/simulator/ActionBar.tsx
@@ -1,15 +1,19 @@
 // Copyright (c) Microsoft Corporation.
 // Licensed under the MIT license.
 
-import RefreshLogo from "../../svgs/refresh_svg";
-import Button from "../Button";
 import * as React from "react";
 import {CONSTANTS} from "../../constants"
+import RefreshLogo from "../../svgs/refresh_svg";
+import Button from "../Button";
+
 interface IProps{
     onTogglePlay: (event: React.MouseEvent<HTMLElement>) => void,
     onToggleRefresh: (event: React.MouseEvent<HTMLElement>) => void,
     playStopImage:JSX.Element
 }
+
+// Component including the actions done on the Simulator (play/stop, refresh)
+
 class ActionBar extends React.Component<IProps>{
     public render(){
         const {onTogglePlay,onToggleRefresh,playStopImage}=this.props;
