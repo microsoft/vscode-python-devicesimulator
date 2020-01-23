5916bf314d56e1ec615006e2d06cfb243b606eca
unknown <vandyliu1999@gmail.com>
2020-01-22 23:18:22 -0800

    initial commit

--- src/simulatorDebugConfigurationProvider.ts
+++ src/simulatorDebugConfigurationProvider.ts
@@ -4,95 +4,103 @@
 import * as vscode from "vscode";
 import { CONSTANTS, DialogResponses } from "./constants";
 import {
-  getServerPortConfig,
-  validCodeFileName
+    getServerPortConfig,
+    validCodeFileName,
 } from "./extension_utils/utils";
 
 let shouldShowInvalidFileNamePopup: boolean = true;
 
 export class SimulatorDebugConfigurationProvider
-  implements vscode.DebugConfigurationProvider {
-  public deviceSimulatorExpressDebug: boolean;
+    implements vscode.DebugConfigurationProvider {
+    public deviceSimulatorExpressDebug: boolean;
 
-  constructor(private pathToScript: string) {
-    this.deviceSimulatorExpressDebug = false;
-  }
-
-  /**
-   * Modify the debug configuration just before a debug session is being launched.
-   */
-  public resolveDebugConfiguration(
-    folder: vscode.WorkspaceFolder | undefined,
-    config: vscode.DebugConfiguration,
-    token?: vscode.CancellationToken
-  ): vscode.ProviderResult<vscode.DebugConfiguration> {
-    const activeTextEditor = vscode.window.activeTextEditor;
-
-    // Create a configuration if no launch.json exists or if it's empty
-    if (!config.type && !config.request && !config.name) {
-      if (
-        activeTextEditor &&
-        activeTextEditor.document.languageId === "python"
-      ) {
-        config.type = "deviceSimulatorExpress";
-        config.request = "launch";
-        config.name = "Device Simulator Express Debugger";
-        config.console = "integratedTerminal";
-      }
+    constructor(private pathToScript: string) {
+        this.deviceSimulatorExpressDebug = false;
     }
-    // Check config type
-    if (config.type === CONSTANTS.DEBUG_CONFIGURATION_TYPE) {
-      this.deviceSimulatorExpressDebug = true;
-      if (activeTextEditor) {
-        const currentFilePath = activeTextEditor.document.fileName;
 
-        // Check file type and name
-        if (!(activeTextEditor.document.languageId === "python")) {
-          return vscode.window
-            .showErrorMessage(CONSTANTS.ERROR.INVALID_FILE_EXTENSION_DEBUG)
-            .then(() => {
-              return undefined; // Abort launch
-            });
-        } else if (
-          !validCodeFileName(currentFilePath) &&
-          shouldShowInvalidFileNamePopup
-        ) {
-          vscode.window
-            .showInformationMessage(
-              CONSTANTS.INFO.INVALID_FILE_NAME_DEBUG,
-              ...[DialogResponses.DONT_SHOW, DialogResponses.MESSAGE_UNDERSTOOD]
-            )
-            .then((selection: vscode.MessageItem | undefined) => {
-              if (selection === DialogResponses.DONT_SHOW) {
-                shouldShowInvalidFileNamePopup = false;
-              }
-            });
+    /**
+     * Modify the debug configuration just before a debug session is being launched.
+     */
+    public resolveDebugConfiguration(
+        folder: vscode.WorkspaceFolder | undefined,
+        config: vscode.DebugConfiguration,
+        token?: vscode.CancellationToken
+    ): vscode.ProviderResult<vscode.DebugConfiguration> {
+        const activeTextEditor = vscode.window.activeTextEditor;
+
+        // Create a configuration if no launch.json exists or if it's empty
+        if (!config.type && !config.request && !config.name) {
+            if (
+                activeTextEditor &&
+                activeTextEditor.document.languageId === "python"
+            ) {
+                config.type = "deviceSimulatorExpress";
+                config.request = "launch";
+                config.name = "Device Simulator Express Debugger";
+                config.console = "integratedTerminal";
+            }
         }
-        // Set the new configuration type so the python debugger can take over
-        config.type = "python";
-        // Set process_user_code path as program
-        config.program = this.pathToScript;
-        // Set user's code path and server's port as args
-        config.args = [currentFilePath, getServerPortConfig().toString()];
-        // Set rules
-        config.rules = [
-          { path: this.pathToScript, include: false },
-          {
-            module: "adafruit_circuitplayground",
-            include: false
-          },
-          { module: "playsound", include: false }
-        ];
-      }
+        // Check config type
+        if (config.type === CONSTANTS.DEBUG_CONFIGURATION_TYPE) {
+            this.deviceSimulatorExpressDebug = true;
+            if (activeTextEditor) {
+                const currentFilePath = activeTextEditor.document.fileName;
+
+                // Check file type and name
+                if (!(activeTextEditor.document.languageId === "python")) {
+                    return vscode.window
+                        .showErrorMessage(
+                            CONSTANTS.ERROR.INVALID_FILE_EXTENSION_DEBUG
+                        )
+                        .then(() => {
+                            return undefined; // Abort launch
+                        });
+                } else if (
+                    !validCodeFileName(currentFilePath) &&
+                    shouldShowInvalidFileNamePopup
+                ) {
+                    vscode.window
+                        .showInformationMessage(
+                            CONSTANTS.INFO.INVALID_FILE_NAME_DEBUG,
+                            ...[
+                                DialogResponses.DONT_SHOW,
+                                DialogResponses.MESSAGE_UNDERSTOOD,
+                            ]
+                        )
+                        .then((selection: vscode.MessageItem | undefined) => {
+                            if (selection === DialogResponses.DONT_SHOW) {
+                                shouldShowInvalidFileNamePopup = false;
+                            }
+                        });
+                }
+                // Set the new configuration type so the python debugger can take over
+                config.type = "python";
+                // Set process_user_code path as program
+                config.program = this.pathToScript;
+                // Set user's code path and server's port as args
+                config.args = [
+                    currentFilePath,
+                    getServerPortConfig().toString(),
+                ];
+                // Set rules
+                config.rules = [
+                    { path: this.pathToScript, include: false },
+                    {
+                        module: "adafruit_circuitplayground",
+                        include: false,
+                    },
+                    { module: "playsound", include: false },
+                ];
+            }
+        }
+        // Abort / show error message if can't find process_user_code.py
+        if (!config.program) {
+            return vscode.window
+                .showErrorMessage(CONSTANTS.ERROR.NO_PROGRAM_FOUND_DEBUG)
+                .then(() => {
+                    return undefined; // Abort launch
+                });
+        }
+        return config;
     }
-    // Abort / show error message if can't find process_user_code.py
-    if (!config.program) {
-      return vscode.window
-        .showErrorMessage(CONSTANTS.ERROR.NO_PROGRAM_FOUND_DEBUG)
-        .then(() => {
-          return undefined; // Abort launch
-        });
-    }
-    return config;
-  }
 }
--- src/view/components/toolbar/SensorButton.tsx
+++ src/view/components/toolbar/SensorButton.tsx
@@ -6,18 +6,18 @@ import "../../styles/SensorButton.css";
 import { ISensorButtonProps } from "../../viewUtils";
 
 const SensorButton: React.FC<ISensorButtonProps> = props => {
-  return (
-    <button
-      onMouseUp={props.onMouseUp}
-      onMouseDown={props.onMouseDown}
-      onKeyUp={props.onKeyUp}
-      onKeyDown={props.onKeyDown}
-      aria-label={`${props.type} sensor button`}
-      className="sensor-button"
-    >
-      {props.label}
-    </button>
-  );
+    return (
+        <button
+            onMouseUp={props.onMouseUp}
+            onMouseDown={props.onMouseDown}
+            onKeyUp={props.onKeyUp}
+            onKeyDown={props.onKeyDown}
+            aria-label={`${props.type} sensor button`}
+            className="sensor-button"
+        >
+            {props.label}
+        </button>
+    );
 };
 
 export default SensorButton;
