b2196a624041ac53b7100a182c9ccc925ec31866
xnkevinnguyen <xuannamkevin@gmail.com>
2020-01-24 08:54:24 -0800

    Add a basic test for App component

--- src/view/App.test.tsx
+++ src/view/App.test.tsx
@@ -1,11 +1,8 @@
 import * as React from 'react'
 import * as ReactDOM from 'react-dom'
 import { IntlProvider } from 'react-intl';
-import App from '../App';
+import App from './App';
 
-test('adds 1 + 2 to equal 3', () => {
-    expect(1+2).toBe(3);
-  });
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
