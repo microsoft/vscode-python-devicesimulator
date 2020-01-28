ef6498cf02e11eb90cf0f9ab018a9e6bca5af2a0
andreamah <andrea_mah22@hotmail.com>
2020-01-28 15:56:15 -0800

    finished first draft of most image methods

--- src/microbit/code_processing_shim.py
+++ src/microbit/code_processing_shim.py
@@ -13,4 +13,12 @@ display = microbit_model.mb.display
 microbit = microbit_model.mb
 Image = image.Image
 
+def repr(image):
+    
+    ret_str = "Image(\'"
+    for index_y in range(0,image.height):
+        ret_str += image.row_to_str(index_y)
+    
+    ret_str = ret_str + "\')"
 
+    return ret_str
--- src/microbit/image.py
+++ src/microbit/image.py
@@ -3,14 +3,25 @@ from . import constants as CONSTANTS
 from . import display
 
 class Image:
-    def __init__(self, pattern = CONSTANTS.BLANK):
+    def __init__(self, *args, **kwargs):
+        print("args")
+        print(args)
         # State in the Python process
-        if type(pattern) is str:
-            self.LED = self.convert_to_array(pattern)
+        if (len(args)==0): 
+            self.LED = CONSTANTS.BLANK
+        elif (len(args)==1):
+            pattern = args[0]
+            if type(pattern) is str:
+                self.LED = self.convert_to_array(pattern)
+            else:
+                self.LED = pattern
         else:
-            self.LED = pattern
+            width = args[0]
+            height = args[1]
+            self.LED = self.create_leds(width,height)
 
-        
+    
+
     def convert_to_array(self, pattern):
         arr = []
         sub_arr = []
@@ -20,6 +31,17 @@ class Image:
                 arr.append(sub_arr)
                 sub_arr = []
             arr.append(sub_arr)
+        return arr
+
+    def create_leds(self, w, h):
+        arr = []
+        for _ in range(0,h):
+            sub_arr = []
+            for _ in range(0,w):
+                sub_arr.append(0)
+
+            arr.append(sub_arr)
+
         return arr
 
 
@@ -36,10 +58,16 @@ class Image:
     def copy(self):
         return Image(self.LED)
         
+    def invert(self,value):
+        for y in range(0,self.height):
+            for x in range(0,self.width):
+                self.set_pixel(x, y, 9-value)
+    
+    
     def fill(self,value):
         for y in range(0,self.height):
             for x in range(0,self.width):
-                self.LED[y][x] = value
+                self.set_pixel(x, y, value)
 
     @property
     def width(self):
@@ -62,4 +90,65 @@ class Image:
         # will name exception later
         raise Exception
 
+    def blit(self, src, x, y, w, h, xdest=0, ydest=0):
+        for count_y in range(0, h):
+            for count_x in range(0, w):
+                if (ydest + count_y < self.height and
+                    xdest + count_x < self.width and
+                    y + count_y < src.height and
+                    x + count_x < src.width):
+                    transfer_pixel = src.get_pixel(x + count_x, y + count_y)
+                    self.set_pixel(xdest + count_x, ydest + count_y, transfer_pixel)
+
+    def crop(self, x, y, w, h):
+        res = Image(w, h)
+        res.blit(self, x, y, w, h)
+        return res
+
+    def shift_vertical(self,n):
+        
+        res = Image(self.width, self.height)
+        if n > 0:
+            # up
+            res.blit(self, 0, n, self.width, self.height-n, 0, 0)
+        else:
+            # down
+            res.blit(self, 0, 0, self.width, self.height-abs(n), 0, abs(n))
+            
+        return res
+
     
+    def shift_horizontal(self,n):
+        res = Image(self.width, self.height)
+        if n > 0:
+            # right
+            res.blit(self, 0, 0, self.width-n, self.height, n, 0)
+        else:
+            # left
+            res.blit(self, n, 0, self.width-n, self.height, 0, 0)
+            
+        return res
+
+
+    def shift_up(self,n):
+        return self.shift_vertical(n)
+
+    def shift_down(self,n):
+        return self.shift_vertical(n*-1)
+
+    def shift_right(self,n):
+        return self.shift_horizontal(n)
+
+    def shift_left(self,n):
+        return self.shift_horizontal(n*-1)
+
+
+    def row_to_str(self, y):
+        new_str = ""
+        for x in range(0,self.width):
+            new_str = new_str + str(self.get_pixel(x,y))
+        
+        new_str = new_str + ":"
+
+        return new_str
+
