dfe868b89412722f711b02d54ac199b86d9ce5bf
unknown <vandyliu1999@gmail.com>
2020-01-29 21:21:16 -0800

    added comments

--- src/microbit/model/microbit.py
+++ src/microbit/model/microbit.py
@@ -7,8 +7,9 @@ class MicrobitModel:
         self.button_a = Button()
         self.button_b = Button()
 
+    # Will be removed once functions added to shim
     def show_message(self, message):
-        print("message!! " + message)
+        print("message: " + message)
 
 
 mb = MicrobitModel()
