c386d7407f7a4c0b931742ac1ebc25b69994ed99
xnkevinnguyen <xuannamkevin@gmail.com>
2020-01-30 11:14:26 -0800

    Add styling for microbit image

--- src/view/components/microbit/Microbit.tsx
+++ src/view/components/microbit/Microbit.tsx
@@ -2,16 +2,20 @@
 // Licensed under the MIT license.
 
 import * as React from "react";
+import "../../styles/Simulator.css";
 import { MicrobitImage } from "./MicrobitImage";
 
-// Component grouping the functionality for circuit playground express
+// Component grouping the functionality for micro:bit functionalities
 
 export class Microbit extends React.Component {
     render() {
         return (
-            <React.Fragment>
-                <MicrobitImage />
-            </React.Fragment>
+            <div className="simulator">
+                <div className="microbit-container">
+                    <MicrobitImage />
+                </div>
+                {/* Implement actionbar here */}
+            </div>
         );
     }
 }
