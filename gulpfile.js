90bd1d070153497ff73e284172777bf76c99c78c
andreamah <andrea_mah22@hotmail.com>
2020-01-30 16:59:27 -0800

    fixed small mistake in gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -35,6 +35,7 @@ gulp.task("clean", () => {
 
 const pythonToMove = [
   "./src/adafruit_circuitplayground/*.*",
+  "./src/microbit/*.*",
   "./src/microbit/!(test)/*.*",
   "./src/*.py",
   "./src/requirements.txt",
