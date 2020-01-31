e13db4c19598d6b5d5c9ddd4d24239b144f5303e
andreamah <andrea_mah22@hotmail.com>
2020-01-30 16:05:40 -0800

    functions and structure implemented for Image class

--- src/microbit/code_processing_shim.py
+++ src/microbit/code_processing_shim.py
@@ -2,49 +2,32 @@ from . import microbit_model
 from . import image
 from . import constants as CONSTANTS
 
-# EXAMPLE
-# can be called simply as "show_message("string")"
-def show_message(message):
-    microbit_model.mb.show_message(message)
-
-
-display = microbit_model.mb.display
-
 microbit = microbit_model.mb
 Image = image.Image
 
+
+# These are methods for image-to-string representation.
 def repr(image):
-    
-    ret_str = "Image(\'"
-    for index_y in range(0,image.height()):
-        ret_str += row_to_str(image, index_y)
-    
-    ret_str = ret_str + "\')"
+
+    ret_str = "Image('"
+    for index_y in range(0, image.height()):
+        ret_str += image.__row_to_str(index_y)
+
+    ret_str = ret_str + "')"
 
     return ret_str
 
 
 def str(image):
     if type(image) is Image:
-        ret_str = "Image(\'\n"
-        for index_y in range(0,image.height()):
-            ret_str += "\t" + row_to_str(image,index_y) + "\n"
-        
-        ret_str = ret_str + "\')"
+        ret_str = "Image('\n"
+        for index_y in range(0, image.height()):
+            ret_str += "\t" + image.__row_to_str(index_y) + "\n"
+
+        ret_str = ret_str + "')"
 
         return ret_str
     else:
         # if not image, call regular str class
         return image.__str__()
 
-
-
-# method to help with string formation
-def row_to_str(image, y):
-    new_str = ""
-    for x in range(0, image.width()):
-        new_str = new_str + str(image.get_pixel(x, y))
-
-    new_str = new_str + ":"
-
-    return new_str
--- src/microbit/image.py
+++ src/microbit/image.py
@@ -1,33 +1,39 @@
 from . import microbit_model
 from . import constants as CONSTANTS
-from . import display
-import copy 
+import copy
+
 
 class Image:
     def __init__(self, *args, **kwargs):
+
+        # Depending on the number of arguments
+        # in constructor, it treat args differently.
+
         if len(args) == 0:
-            self.__LED = copy.deepcopy(CONSTANTS.BLANK)
+            # default constructor
+            self.__LED = self.__string_to_array(CONSTANTS.BLANK)
         elif len(args) == 1:
             pattern = args[0]
             if type(pattern) is str:
                 self.__LED = self.__string_to_array(pattern)
             else:
-                self.__LED = pattern
+                raise TypeError("Image(s) takes a string")
         else:
 
             width = args[0]
             height = args[1]
 
             if width < 0 or height < 0:
-                # not in original, but ideally,
+                # This is not in original, but ideally,
                 # image should fail non-silently
                 raise ValueError(CONSTANTS.INDEX_ERR)
-            if (len(args) == 3):
-                byte_arr = args[2]
-                self.__LED = self.__bytes_to_array(width,height,byte_arr)
-            else:
-                self.__LED = self.__create_leds(width,height)
 
+            if len(args) == 3:
+                # This option is for potential third bytearray arguments
+                byte_arr = args[2]
+                self.__LED = self.__bytes_to_array(width, height, byte_arr)
+            else:
+                self.__LED = self.__create_leds(width, height)
 
     def width(self):
         if len(self.__LED):
@@ -50,16 +56,16 @@ class Image:
             print(CONSTANTS.COPY_ERR_MESSAGE)
 
     def get_pixel(self, x, y):
-        if self.__valid_pos(x,y):
+        if self.__valid_pos(x, y):
             return self.__LED[y][x]
         else:
             raise ValueError(CONSTANTS.INDEX_ERR)
 
     def shift_up(self, n):
-        return self.__shift_vertical(n)
+        return self.__shift_vertical(n * -1)
 
     def shift_down(self, n):
-        return self.__shift_vertical(n * -1)
+        return self.__shift_vertical(n)
 
     def shift_right(self, n):
         return self.__shift_horizontal(n)
@@ -73,31 +79,39 @@ class Image:
         return res
 
     def copy(self):
-        return Image(self.__LED)
+        return Image(self.__create_string())
 
-    def invert(self, value):
+    # This inverts the brightness of each LED.
+    # ie: pixel that is at brightness 4 would become brightness 5
+    # and pixel that is at brightness 9 would become brightness 0.
+    def invert(self):
         for y in range(0, self.height()):
             for x in range(0, self.width()):
-                self.set_pixel(x, y, 9 - value)
+                self.set_pixel(x, y, 9 - self.get_pixel(x, y))
 
+    # This fills all LEDs with same brightness.
     def fill(self, value):
         for y in range(0, self.height()):
             for x in range(0, self.width()):
                 self.set_pixel(x, y, value)
 
-
+    # This transposes certain area (w x h) on src onto current image.
     def blit(self, src, x, y, w, h, xdest=0, ydest=0):
-        
-        if (not self.__valid_pos(x,y) or not src.__valid_pos(xdest, ydest)):
-                raise ValueError(CONSTANTS.INDEX_ERR)
+
+        if not src.__valid_pos(x, y):
+            raise ValueError(CONSTANTS.INDEX_ERR)
 
         for count_y in range(0, h):
             for count_x in range(0, w):
-                if (self.__valid_pos(xdest + count_x, ydest + count_y) and
-                    src.__valid_pos(x + count_x, y + count_y)):
-                    transfer_pixel = src.get_pixel(x + count_x, y + count_y)
+                if self.__valid_pos(xdest + count_x, ydest + count_y):
+                    if src.__valid_pos(x + count_x, y + count_y):
+                        transfer_pixel = src.get_pixel(x + count_x, y + count_y)
+                    else:
+                        transfer_pixel = 0
                     self.set_pixel(xdest + count_x, ydest + count_y, transfer_pixel)
 
+    # This adds two images (if other object is not an image, throws error).
+    # The images must be the same size.
     def __add__(self, other):
         if not (type(other) is Image):
             raise TypeError(
@@ -107,7 +121,7 @@ class Image:
             raise ValueError(CONSTANTS.SAME_SIZE_ERR)
         else:
             res = Image(self.width(), self.height())
-    
+
             for y in range(0, self.height()):
                 for x in range(0, self.width()):
                     sum = other.get_pixel(x, y) + self.get_pixel(x, y)
@@ -116,8 +130,13 @@ class Image:
 
             return res
 
+    # This multiplies image by number (if other factor is not a number, it throws an error).
     def __mul__(self, other):
-        float_val = float(other)
+        try:
+            float_val = float(other)
+        except TypeError:
+            raise TypeError(f"can't convert {type(other)} to float")
+
         res = Image(self.width(), self.height())
 
         for y in range(0, self.height()):
@@ -127,50 +146,76 @@ class Image:
 
         return res
 
-    # helpers!
+    # HELPER FUNCTIONS
 
+    # This create 2D array of off LEDs with
+    # width w and height h
     def __create_leds(self, w, h):
         arr = []
-        for _ in range(0,h):
+        for _ in range(0, h):
             sub_arr = []
-            for _ in range(0,w):
+            for _ in range(0, w):
                 sub_arr.append(0)
             arr.append(sub_arr)
-        return arr
-        
-    
-    def __bytes_to_array(self, height, width, byte_arr):
-        bytes_translated  = bytes(byte_arr)
 
-        if (not (len(bytes_translated)) == height*width):
+        return arr
+
+    # This turns byte array to 2D array for LED field.
+    def __bytes_to_array(self, height, width, byte_arr):
+        bytes_translated = bytes(byte_arr)
+
+        if not (len(bytes_translated)) == height * width:
             raise ValueError(CONSTANTS.INCORR_IMAGE_SIZE)
-        
+
         arr = []
         sub_arr = []
 
-        for index,elem in enumerate(bytes_translated):
-            if index % width == 0 and not index is 0:
+        for index, elem in enumerate(bytes_translated):
+            if index % width == 0 and index != 0:
                 arr.append(sub_arr)
                 sub_arr = []
 
             sub_arr.append(elem)
-        
+
         arr.append(sub_arr)
         return arr
 
-
-
+    # This converts string (with different rows separated by ":")
+    # to 2d array arrangement.
     def __string_to_array(self, pattern):
+        initial_array, max_subarray_len = self.__string_to_initial_array(pattern)
+
+        # Fill in empty spaces in w x h matrix.
+        for arr_y in initial_array:
+            num_extra_spaces = max_subarray_len - len(arr_y)
+            for _ in range(num_extra_spaces):
+                arr_y.append(0)
+
+        return initial_array
+
+    def __string_to_initial_array(self, pattern):
+        # The result may have spaces in the 2D array
+        # and may unevent sub-array lengths
         arr = []
         sub_arr = []
+
+        max_subarray_len = 0
+
         for elem in pattern:
             if elem == ":":
+                if len(sub_arr) > max_subarray_len:
+                    max_subarray_len = len(sub_arr)
                 arr.append(sub_arr)
                 sub_arr = []
             else:
                 sub_arr.append(int(elem))
-        return arr
 
+        if len(pattern) and not str(pattern)[-1] == ":":
+            arr.append(sub_arr)
+
+        return arr, max_subarray_len
+
+    # This returns the limit if the result is too big.
     def __limit_result(self, limit, result):
         if result > limit:
             return limit
@@ -180,19 +225,20 @@ class Image:
     def __valid_brightness(self, value):
         return 0 <= value and value <= 9
 
-
     def __valid_pos(self, x, y):
         return 0 <= x and x < self.width() and 0 <= y and y < self.height()
 
     def __shift_vertical(self, n):
 
         res = Image(self.width(), self.height())
+
         if n > 0:
-            # up
-            res.blit(self, 0, n, self.width(), self.height() - n, 0, 0)
-        else:
             # down
-            res.blit(self, 0, 0, self.width(), self.height() - abs(n), 0, abs(n))
+            res.blit(self, 0, 0, self.width(), self.height() - n, 0, n)
+        else:
+            # up
+            if self.__valid_pos(0, abs(n)):
+                res.blit(self, 0, abs(n), self.width(), self.height() - abs(n), 0, 0)
 
         return res
 
@@ -203,6 +249,22 @@ class Image:
             res.blit(self, 0, 0, self.width() - n, self.height(), n, 0)
         else:
             # left
-            res.blit(self, n, 0, self.width() - n, self.height(), 0, 0)
+            if self.__valid_pos(abs(n), 0):
+                res.blit(self, abs(n), 0, self.width() - abs(n), self.height(), 0, 0)
 
         return res
+
+    def __create_string(self):
+        ret_str = ""
+        for index_y in range(0, self.height()):
+            ret_str += self.__row_to_str(index_y)
+        return ret_str
+
+    def __row_to_str(self, y):
+        new_str = ""
+        for x in range(0, self.width()):
+            new_str = new_str + str(self.get_pixel(x, y))
+
+        new_str = new_str + ":"
+
+        return new_str
