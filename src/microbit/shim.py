7d04635203a7a6afdee2850785773924e15fbfc4
andreamah <andrea_mah22@hotmail.com>
2020-02-04 11:54:14 -0800

    shim adjustement

--- src/microbit/shim.py
+++ src/microbit/shim.py
@@ -1,12 +1,9 @@
 from .model import microbit_model
-<<<<<<< HEAD
 from .model import image
 
 microbit = microbit_model.mb
 Image = image.Image
 display = microbit.display
-=======
->>>>>>> users/t-xunguy/leds
 
 
 def sleep(n):
