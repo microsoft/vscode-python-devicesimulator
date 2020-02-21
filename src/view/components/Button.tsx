e58caf69f25d23853d2100f341ed267b7ba1529a
xnkevinnguyen <xuannamkevin@gmail.com>
2020-02-21 15:02:34 -0800

    Remove changes on button component

--- src/view/components/Button.tsx
+++ src/view/components/Button.tsx
@@ -1,7 +1,5 @@
 import * as React from "react";
 import "../styles/Button.css";
-import { ViewStateContext } from "../context";
-import { VIEW_STATE } from "../constants";
 
 export interface IButtonProps {
     label: string;
@@ -17,8 +15,6 @@ const Button: React.FC<IButtonProps> = props => {
     const iconSvg: SVGElement = props.image as SVGElement;
     const buttonStyle = { width: props.width };
     const tabIndex = props.focusable ? 0 : -1;
-    const isButtonDisabled =
-        React.useContext(ViewStateContext) === VIEW_STATE.PAUSE;
 
     return (
         <button
@@ -29,7 +25,6 @@ const Button: React.FC<IButtonProps> = props => {
             onClick={props.onClick}
             style={buttonStyle}
             tabIndex={tabIndex}
-            disabled={isButtonDisabled}
         >
             {iconSvg}
         </button>
