71f50fd54c4340237ce431d1dd306e60a4d5ae60
andreamah <andrea_mah22@hotmail.com>
2020-02-25 11:27:13 -0800

    update comments for clarity

--- src/check_if_venv.py
+++ src/check_if_venv.py
@@ -5,4 +5,7 @@ isVenv = hasattr(sys, "real_prefix") or (
     hasattr(sys, "base_prefix") and sys.base_prefix != sys.prefix
 )
 
+# prints result for frontend to read
+# 1 -> is a venv
+# 0 -> is NOT a venv
 print(int(isVenv))
