4afc2e02421cef51e8b7ab378200e076282569cb
andreamah <andrea_mah22@hotmail.com>
2020-02-26 12:18:39 -0800

    fixed title

--- src/service/PopupService.ts
+++ src/service/PopupService.ts
@@ -5,8 +5,8 @@ import { LATEST_RELEASE_NOTE } from "../latest_release_note";
 export class PopupService {
     public static OPEN_RELEASE_NOTE() {
         const panel = vscode.window.createWebviewPanel(
-            "catCoding",
-            "Cat Coding",
+            "releaseNote",
+            "Release Note",
             vscode.ViewColumn.One,
             {}
         );
