c66967b3bc6a1adaa270979042f0a575c35c8bae
xnkevinnguyen <xuannamkevin@gmail.com>
2020-03-05 15:32:31 -0800

    Fix tests for mac os

--- src/adafruit_circuitplayground/test/test_utils.py
+++ src/adafruit_circuitplayground/test/test_utils.py
@@ -13,16 +13,21 @@ class TestUtils(object):
         assert expected == utils.remove_leading_slashes(original)
 
     def test_escape_notOSX(self):
+        _utils_sys = utils.sys
         if sys.platform.startswith(CONSTANTS.MAC_OS):
             utils.sys = mock.MagicMock()
             utils.sys.configure_mock(platform="win32")
         original = "a b"
         assert original == utils.escape_if_OSX(original)
+        utils.sys = _utils_sys
 
     def test_escape_isOSX(self):
+        _utils_sys = utils.sys
         if not sys.platform.startswith(CONSTANTS.MAC_OS):
             utils.sys = mock.MagicMock()
             utils.sys.configure_mock(platform="darwin")
         original = "a b"
         expected = "a%20b"
         assert expected == utils.escape_if_OSX(original)
+        utils.sys = _utils_sys
+
