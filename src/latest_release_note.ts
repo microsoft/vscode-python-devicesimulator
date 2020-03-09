3839e0b78c4d212c455abf5ee4fd7382d8e680c6
andreamah <andrea_mah22@hotmail.com>
2020-03-09 11:43:21 -0700

    release note modification

--- src/latest_release_note.ts
+++ src/latest_release_note.ts
@@ -1,7 +1,8 @@
 // TODO: find a better way of loading html into a string
-export const LATEST_RELEASE_NOTE = `<h1>Device Simulator Express Release Notes 👩🏾‍💻 👨🏾‍💻 (Mar. 3, 2020)</h1>
+export const LATEST_RELEASE_NOTE = `<h1>Device Simulator Express Release Notes 🐍📝 (Mar. 10, 2020)</h1>
 <p>
-    Welcome to the first update to the Device Simulator Express! <u>Please feel free to enable our feature flag in
+    Thanks for using our extension! We're back with some small updates. <u>Again, please feel free to enable our feature
+        flag in
         Settings
         (under the setting titled “<b>deviceSimulatorExpress.previewMode</b>” in the User settings)</u>.
 </p>
@@ -9,59 +10,19 @@ export const LATEST_RELEASE_NOTE = `<h1>Device Simulator Express Release Notes �
 <p>
     <h3>Fixes (enabled by default):</h3>
     <ul>
-        <li>Enabled support for “from adafruit_circuitplayground import cp” as an import statement for the CPX and
-            changed
-            “New File” template to use this format.</li>
-        <ul>
-            <li>Aligns better with newer online tutorials. Best practice for adafruit_circuitplayground library imports
-                changed with <a href="https://github.com/adafruit/Adafruit_CircuitPython_CircuitPlayground/pull/79">this
-                    PR
-                    in Adafruit’s official repo</a>.</li>
-        </ul>
-        <li>State for sensor selection persists.</li>
-        <li>More reliable dependency installation and more informative setup fail information.</li>
-        <li>Fixes to Serial Monitor for CPX device deployment.</li>
-        <li>More robust debugger functionality.</li>
-        <li>Fixed spelling and clarity errors in documentation and pop-up messages.</li>
+        <li>Playing sounds on the CPX simulation now works on MacOS!
+            <ul>
+                <li>Try it out by using <i>cp.play_file(PATH_TO_FILE)</i>.</li>
+            </ul>
+        </li>
+        <li>Previously, the play button on the simulations would sometimes require two clicks. This was fixed.</li>
+        <li>The editor can now support unicode characters (such as Japanese).</li>
     </ul>
     <h3>New features (only available with feature flag enabled):</h3>
     <ul>
-        <li><u>BBC micro:bit simulator and debugger – <i>open up a new micro:bit file, write code for the micro:bit and
-                    test it out!</u></i>
-            <ul>
-                <li>Ability to interact with LEDs, buttons, and sensors.</li>
-                <li>Includes autocompletion and error flagging.</li>
-                <li>Supports the following:</li>
-                <ul>
-                    <li>Classes:
-                        <ul>
-                            <li>display</li>
-                            <li>image</li>
-                            <li>accelerometer</li>
-                            <li>button</li>
-                        </ul>
-                </ul>
-                <ul>
-                    <li>Global static functions:</li>
-                    <ul>
-                        <li>sleep()</li>
-                        <li>running_time()</li>
-                        <li>temperature()</li>
-                    </ul>
-                </ul>
-            </ul>
-            <ul>
-                <li>Includes accessibility considerations for simulation.</li>
-                <ul>
-                    <li>Has ability to use keyboard for button presses and navigation.</li>
-                </ul>
-            </ul>
+        <li>BBC micro:bit deploy to device and accompanying serial monitor support.
     </ul>
 </p>
-<h2>Upcoming Improvements</h2>
-<ul>
-    <li>Deploying to device on the micro:bit with serial monitor interaction.</li>
-</ul>
 <br>
-<p><b>Happy Hacking! ✨✨🐍🐍🍰</b><br>
+<p><b>Keep being a programming pro 😎🔋,</b><br>
     &nbsp&nbsp&nbsp&nbsp&nbsp <b><i>- The Device Simulator Express Team</i></b></p>`;
