c9bf221d591f98ea9d14501165045a4934cb48d1
xnkevinnguyen <xuannamkevin@gmail.com>
2020-03-13 09:46:40 -0700

    Initialize html page for getting started

--- src/pages/gettingStarted.ts
+++ src/pages/gettingStarted.ts
@@ -1,1 +1,63 @@
-export const GETTING_STARTED_HTML = "";
+export const GETTING_STARTED_HTML = `<!DOCTYPE html>
+    <html>
+        <head>
+            <title></title>
+        </head>
+        <style>
+            .inv {
+                display: none;
+            }
+            .deviceSelector {
+                width: 250px;
+                border: 1px solid #3D484C;
+                margin: 0 0 5px;
+                padding: 8px;
+                border-radius: 5px;
+                font-size: 12px;
+                padding-right: 30px;
+            }
+            .codeBox {
+                display: block;
+                width: 90%; height: 50px;
+                margin: 10px;
+                padding: 10px;
+                text-align: left;
+                background: none;
+                border: 1px solid grey;
+                border-radius:4px;
+            }
+
+        </style>
+        <body>
+            <h1>Getting started</h1>
+
+            <select id="target" class="deviceSelector">
+                <option selected disabled>Select a Device</option>
+                <option value="micro:bit">micro:bit</option>
+                <option value="CPX">CPX</option>
+            <select>
+    
+            <div id="micro:bit" class="visibleElement">
+                <h2> Tutorial for micro:bit </h2>
+                <h3> 1. Light up your micro:bit with love by showing a heart.</h3>
+                <span class="codeBox">display.show(Image.HEART)</span>
+            </div>
+            <div id="CPX" class="inv">Content 2</div>
+    
+            <script>
+                document
+                    .getElementById('target')
+                    .addEventListener('change', function () {
+                        'use strict';
+                        var visibleElement = document.querySelector('.visibleElement'),   
+                            target = document.getElementById(this.value);
+                        if (visibleElement !== null) {
+                            visibleElement.className = 'inv';
+                        }
+                        if (target !== null ) {
+                            target.className = 'visibleElement';
+                        }
+                });
+            </script>
+        </body>
+    </html>`;
