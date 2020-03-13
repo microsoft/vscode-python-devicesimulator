86fc862c2d2029b4c44b67af236abedd721a04bc
xnkevinnguyen <xuannamkevin@gmail.com>
2020-03-13 15:01:34 -0700

    Complete tutorials for microbit

--- src/pages/gettingStarted.ts
+++ src/pages/gettingStarted.ts
@@ -39,26 +39,36 @@ export const GETTING_STARTED_HTML = `<!DOCTYPE html>
     
             <div id="micro:bit" class="visibleElement">
                 <h2> Tutorial for micro:bit </h2>
-                <h3> 1. Import the micro:bit library to use it!</h3>
-                <span class="codeBox">display.show(Image.HEART)</span>
-                <h3> 1. Light up your micro:bit with love by showing a heart.</h3>
-                <span class="codeBox">display.show(Image.HEART)</span>
-                <h3> 1. Light up your micro:bit with love by showing a heart.</h3>
-                <span class="codeBox">display.show(Image.HEART)</span>
-                <h3> 1. Light up your micro:bit with love by showing a heart.</h3>
-                <span class="codeBox">display.show(Image.HEART)</span>
-                <h3> 1. Light up your micro:bit with love by showing a heart.</h3>
-                <span class="codeBox">display.show(Image.HEART)</span>
-                <h3> 1. Light up your micro:bit with love by showing a heart.</h3>
-                <span class="codeBox">display.show(Image.HEART)</span>
-                <h3> 1. Light up your micro:bit with love by showing a heart.</h3>
-                <span class="codeBox">display.show(Image.HEART)</span>
-                <h3> 1. Light up your micro:bit with love by showing a heart.</h3>
-                <span class="codeBox">display.show(Image.HEART)</span>
-                <h3> 1. Light up your micro:bit with love by showing a heart.</h3>
-                <span class="codeBox">display.show(Image.HEART)</span>
-                <h3> 1. Light up your micro:bit with love by showing a heart.</h3>
-                <span class="codeBox">display.show(Image.HEART)</span>
+                <h3> 1. Import the micro:bit library to use it! (This is required)</h3>
+                <span class="codeBox">
+                    <pre>from microbit import *</pre>
+                </span>
+                <h3> 2. Light up your micro:bit with love by showing a heart.</h3>
+                <span class="codeBox">
+                    <pre>display.show(Image.HEART)</pre>
+                </span>                
+                <h3> 3. Use your micro:bit to tell the world how you’re feeling.
+                </h3>
+                <span class="codeBox">
+                    <pre>while True:</pre>
+                    <pre>   if button_a.is_pressed():</pre>
+                    <pre>      display.show(Image.HAPPY)</pre>
+                    <pre>   if button_b.is_pressed():</pre>
+                    <pre>       display.show(Image.SAD)</pre>
+                </span>
+                <h3> 4. Read then display the temperature.</h3>
+                <span class="codeBox">
+                    <pre>while True:</pre>
+                    <pre>    temp = temperature()</pre>
+                    <pre>    display.show(temp)</pre>
+                </span>
+                <h3> And much more! These links have more tutorials:</h3>
+                <h3>
+                    <a href="https://microbit.org/projects/make-it-code-it/">Microbit Tutorials</a>
+                </h3>
+                <h3>
+                    <a href="https://microbit-micropython.readthedocs.io/">Microbit official documentation</a>
+                </h3>
             </div>
             <div id="CPX" class="inv">
                 <h2> Tutorial for CPX </h2>
