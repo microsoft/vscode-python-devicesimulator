14ab65a81fedacf99ddfd50977bd2f76dbf0317a
andreamah <andrea_mah22@hotmail.com>
2020-03-20 16:13:03 -0700

    changed release note

--- src/latest_release_note.ts
+++ src/latest_release_note.ts
@@ -1,28 +1,23 @@
 // TODO: find a better way of loading html into a string
-export const LATEST_RELEASE_NOTE = `<h1>Device Simulator Express Release Notes 🐍📝 (Mar. 10, 2020)</h1>
+export const LATEST_RELEASE_NOTE = `<h1>Device Simulator Express Release Notes 🍰🐍 (Mar. 24, 2020)</h1>
 <p>
-    Thanks for using our extension! We're back with some small updates. <u>Again, please feel free to enable our feature
-        flag in
-        Settings
-        (under the setting titled “<b>deviceSimulatorExpress.previewMode</b>” in the User settings)</u>.
-</p>
+    Thanks for using our extension! <b>Support for micro:bit simulation is now officially released on the
+        Device Simulator Express</b>. 💖✨</p>
 <h2>Changes</h2>
 <p>
-    <h3>Fixes (enabled by default):</h3>
+    <h3>Fixes:</h3>
     <ul>
-        <li>Playing sounds on the CPX simulation now works on MacOS!
-            <ul>
-                <li>Try it out by using <i>cp.play_file(PATH_TO_FILE)</i>.</li>
-            </ul>
-        </li>
-        <li>Previously, the play button on the simulations would sometimes require two clicks. This was fixed.</li>
-        <li>The editor can now support unicode characters (such as Japanese).</li>
+        <li>Fixed serial port selection for serial monitor.</li>
+        <li>Fixed debugger runtime issue that appeared on select machines.</li>
     </ul>
-    <h3>New features (only available with feature flag enabled):</h3>
+    <h3>New features:</h3>
     <ul>
-        <li>BBC micro:bit deploy to device and accompanying serial monitor support.</li>
+        <li>Removed feature flag for the micro:bit simulator!</li>
+        <li>New "Getting Started" page containing CPX and micro:bit example code.</li>
+        <li>Tab UI now has callout for "about" information on each sensor/input.</li>
+        <li>Removed the simulator dropdown to only indicate the file being run.</li>
     </ul>
 </p>
 <br>
-<p><b>Keep being a programming pro 😎🔋,</b><br>
-    &nbsp&nbsp&nbsp&nbsp&nbsp <b><i>- The Device Simulator Express Team</i></b></p>`;
+<p><b>Stay healthy while you hack 🥰🌼,</b><br>
+    &nbsp&nbsp&nbsp&nbsp&nbsp <b><i>- The Device Simulator Express Team</i></b></p>`
