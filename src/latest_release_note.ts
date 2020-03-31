9f215b8e87196a849e4e1dac5d5feffca6cd6219
andreamah <andrea_mah22@hotmail.com>
2020-03-31 11:46:03 -0700

    changed release note for hotfix

--- src/latest_release_note.ts
+++ src/latest_release_note.ts
@@ -1,28 +1,21 @@
 // TODO: find a better way of loading html into a string
-export const LATEST_RELEASE_NOTE = `<h1>Device Simulator Express Release Notes 🐍📝 (Mar. 10, 2020)</h1>
+export const LATEST_RELEASE_NOTE = `<h1>Device Simulator Express Release Notes 🐍 (Mar. 31, 2020)</h1>
 <p>
-    Thanks for using our extension! We're back with some small updates. <u>Again, please feel free to enable our feature
-        flag in
-        Settings
-        (under the setting titled “<b>deviceSimulatorExpress.previewMode</b>” in the User settings)</u>.
+    We're back with a small hotfix!</p>
+<p>
+    <h2>Fixes:</h2>
+    <ul>
+        <li>Fixed serial port selection for serial monitor.</li>
+        <li>Fixed launch issue on Ubuntu.</li>
+    </ul>
+
+
 </p>
-<h2>Changes</h2>
 <p>
-    <h3>Fixes (enabled by default):</h3>
-    <ul>
-        <li>Playing sounds on the CPX simulation now works on MacOS!
-            <ul>
-                <li>Try it out by using <i>cp.play_file(PATH_TO_FILE)</i>.</li>
-            </ul>
-        </li>
-        <li>Previously, the play button on the simulations would sometimes require two clicks. This was fixed.</li>
-        <li>The editor can now support unicode characters (such as Japanese).</li>
-    </ul>
-    <h3>New features (only available with feature flag enabled):</h3>
-    <ul>
-        <li>BBC micro:bit deploy to device and accompanying serial monitor support.</li>
-    </ul>
+    <h2>Changes:</h2>
+    <li>Due to necessary serial monitor fixes, <b>the extension is now only supported on VS Code v1.43+</b></li>
+
 </p>
 <br>
-<p><b>Keep being a programming pro 😎🔋,</b><br>
+<p><b>Stay healthy while you hack 🥰🌼,</b><br>
     &nbsp&nbsp&nbsp&nbsp&nbsp <b><i>- The Device Simulator Express Team</i></b></p>`;
