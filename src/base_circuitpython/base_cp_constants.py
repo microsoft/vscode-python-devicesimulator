fab9d9d1e392b8e66d0cc2df3cbfa0608efce147
unknown <vandyliu1999@gmail.com>
2020-04-01 14:40:07 -0700

    first commit

--- src/base_circuitpython/base_cp_constants.py
+++ src/base_circuitpython/base_cp_constants.py
@@ -7,4 +7,6 @@ CLUE_PIN = "D18"
 IMG_DIR_NAME = "img"
 SCREEN_HEIGHT_WIDTH = 240
 
-EXPECTED_INPUT_BUTTONS = ["button_a", "button_b"]
+EXPECTED_INPUT_BUTTONS = set(["button_a", "button_b"])
+
+ALL_EXPECTED_INPUT_EVENTS = set([])
