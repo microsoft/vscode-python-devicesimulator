9d569da2617a116c00014d2828041d778d195e93
Andrea Mah <31675041+andreamah@users.noreply.github.com>
2020-04-02 12:11:03 -0700

    Clue Terminal and Context Fixes (#290)
    Terminal and Screen Updating Fix

--- src/base_circuitpython/board.py
+++ src/base_circuitpython/board.py
@@ -2,12 +2,27 @@
 # https://learn.adafruit.com/arduino-to-circuitpython/the-board-module
 
 
+import terminal_handler
+
+
 class Display:
     def __init__(self):
-        pass
+        self.active_group = None
+        self.terminal = terminal_handler.Terminal()
 
-    def show(self, group):
-        group.draw()
+    def show(self, group=None):
+        if group != self.active_group:
+            self.active_group = group
+
+            if group == None:
+                self.terminal.draw()
+                return
+
+            # if the group has no attribute called
+            # "draw", then it is liable for updating itself
+            # when it calls show
+            if hasattr(group, "draw"):
+                group.draw()
 
 
 DISPLAY = Display()
