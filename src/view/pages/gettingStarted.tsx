0ef85533fcbbabd49f2980b5bd4ddc064edce758
unknown <vandyliu1999@gmail.com>
2020-04-02 16:08:11 -0700

    formatted

--- src/view/pages/gettingStarted.tsx
+++ src/view/pages/gettingStarted.tsx
@@ -55,13 +55,13 @@ export class GettingStartedPage extends React.Component {
                         <h3> 2. Turn on the little red LED</h3>
                         <span className="codeBox">
                             <pre>while True:</pre>
-                            <pre>    cp.red_led = True</pre>
+                            <pre> cp.red_led = True</pre>
                         </span>
                         <h3> 3. Turn up red LED when button A is clicked</h3>
                         <span className="codeBox">
                             <pre>while True:</pre>
-                            <pre>    if cp.button_a:</pre>
-                            <pre>        cp.red_led = True</pre>
+                            <pre> if cp.button_a:</pre>
+                            <pre> cp.red_led = True</pre>
                         </span>
                         <h3> 4. Light up the first neopixel blue</h3>
                         <span className="codeBox">
@@ -101,21 +101,24 @@ export class GettingStartedPage extends React.Component {
                         </h3>
                         <span className="codeBox">
                             <pre>while True:</pre>
-                            <pre>    if button_a.is_pressed():</pre>
-                            <pre>        display.show(Image.HAPPY)</pre>
-                            <pre>    if button_b.is_pressed():</pre>
-                            <pre>        display.show(Image.SAD)</pre>
+                            <pre> if button_a.is_pressed():</pre>
+                            <pre> display.show(Image.HAPPY)</pre>
+                            <pre> if button_b.is_pressed():</pre>
+                            <pre> display.show(Image.SAD)</pre>
                         </span>
                         <h3> 4. Read then display the temperature</h3>
                         <span className="codeBox">
                             <pre>while True:</pre>
-                            <pre>    temp = temperature()</pre>
-                            <pre>    display.show(temp)</pre>
+                            <pre> temp = temperature()</pre>
+                            <pre> display.show(temp)</pre>
                         </span>
-                        <h3> 5. Display your name with the scroll functionality</h3>
+                        <h3>
+                            {" "}
+                            5. Display your name with the scroll functionality
+                        </h3>
                         <span className="codeBox">
                             <pre>while True:</pre>
-                            <pre>    display.show("Your name")</pre>
+                            <pre> display.show("Your name")</pre>
                         </span>
                         <h3>And much more! These links have more tutorials:</h3>
                         <h3>
@@ -132,32 +135,51 @@ export class GettingStartedPage extends React.Component {
                     <div id="CLUE" className="inv">
                         <h2> Tutorial for CLUE </h2>
                         <h3>
-                            1. Import the the main CLUE library (This is required)
+                            1. Import the the main CLUE library (This is
+                            required)
                         </h3>
                         <span className="codeBox">
                             <pre>from adafruit_clue import clue</pre>
                         </span>
-                        <h3> 2. Display text on the CLUE and change the text when a button is pressed</h3>
+                        <h3>
+                            {" "}
+                            2. Display text on the CLUE and change the text when
+                            a button is pressed
+                        </h3>
                         <span className="codeBox">
-                            <pre>clue_data = clue.simple_text_display(title="CLUE!", text_scale=2)</pre>
+                            <pre>
+                                clue_data =
+                                clue.simple_text_display(title="CLUE!",
+                                text_scale=2)
+                            </pre>
                             <pre>while True:</pre>
-                            <pre>    clue_data[1].text = "Hello World!"</pre>
-                            <pre>    clue_data[3].text = "Temperature: {}".format(clue.temperature)</pre>
-                            <pre>    if clue.button_a:</pre>
-                            <pre>        clue_data[5].text = "A is pressed!"</pre>
-                            <pre>    else:</pre>
-                            <pre>        clue_data[5].text = "A is not pressed!"</pre>
-                            <pre>    clue_data.show()</pre>
+                            <pre> clue_data[1].text = "Hello World!"</pre>
+                            <pre>
+                                {" "}
+                                clue_data[3].text = "Temperature: {}
+                                ".format(clue.temperature)
+                            </pre>
+                            <pre> if clue.button_a:</pre>
+                            <pre> clue_data[5].text = "A is pressed!"</pre>
+                            <pre> else:</pre>
+                            <pre> clue_data[5].text = "A is not pressed!"</pre>
+                            <pre> clue_data.show()</pre>
                         </span>
                         <h3> 3. Create a slide show on the CLUE</h3>
-                        <p>There must be some .bmp photos in the same directory as the code file</p>
+                        <p>
+                            There must be some .bmp photos in the same directory
+                            as the code file
+                        </p>
                         <span className="codeBox">
                             <pre>import board</pre>
                             <pre>from adafruit_slideshow import SlideShow</pre>
                             <pre> </pre>
-                            <pre>slideshow = SlideShow(board.DISPLAY, auto_advance=True)</pre>
+                            <pre>
+                                slideshow = SlideShow(board.DISPLAY,
+                                auto_advance=True)
+                            </pre>
                             <pre>while slideshow.update():</pre>
-                            <pre>     pass</pre>
+                            <pre> pass</pre>
                         </span>
                         <h3> 4. Light up the back neopixel green</h3>
                         <span className="codeBox">
@@ -167,12 +189,16 @@ export class GettingStartedPage extends React.Component {
                         <span className="codeBox">
                             <pre>import board</pre>
                             <pre>import displayio</pre>
-                            <pre>from adafruit_display_shapes.rect import Rect</pre>
+                            <pre>
+                                from adafruit_display_shapes.rect import Rect
+                            </pre>
                             <pre> </pre>
                             <pre>splash = displayio.Group(max_size=20)</pre>
                             <pre>board.DISPLAY.show(splash)</pre>
                             <pre> </pre>
-                            <pre>rect = Rect(80, 20, 41, 41, fill=0x0000FF)</pre>
+                            <pre>
+                                rect = Rect(80, 20, 41, 41, fill=0x0000FF)
+                            </pre>
                             <pre>splash.append(rect)</pre>
                         </span>
                         <h3>And much more! These links have more tutorials:</h3>
