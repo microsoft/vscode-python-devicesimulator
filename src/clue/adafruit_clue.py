03b721613d473231bb6c097178d3dadadeead195
unknown <vandyliu1999@gmail.com>
2020-04-06 11:39:23 -0700

    fixed bug in update_button

--- src/clue/adafruit_clue.py
+++ src/clue/adafruit_clue.py
@@ -735,7 +735,7 @@ class Clue:  # pylint: disable=too-many-instance-attributes, too-many-public-met
             CONSTANTS.CLUE_STATE.BUTTON_B: "B",
         }
         if value:
-            self.__state[button].add(buttonMapping[button])
+            self.__state[CONSTANTS.CLUE_STATE.PRESSED_BUTTONS].add(buttonMapping[button])
         self.__state[button] = value
 
 
