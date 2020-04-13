3ce9b36582da12e2853399fa57ca121cc873ab75
andreamah <andrea_mah22@hotmail.com>
2020-04-13 16:04:56 -0700

    fixed group max_size and clue text scaling

--- src/base_circuitpython/displayio/constants.py
+++ src/base_circuitpython/displayio/constants.py
@@ -4,6 +4,7 @@ TILE_OUT_OF_BOUNDS = "Tile index out of bounds"
 INCORR_SUBCLASS = "Layer must be a Group or TileGrid subclass."
 LAYER_ALREADY_IN_GROUP = "Layer already in a group."
 GROUP_FULL = "Group Full"
+SCALE_TOO_SMALL = "scale must be >= 1"
 
 SCREEN_HEIGHT_WIDTH = 240
 
